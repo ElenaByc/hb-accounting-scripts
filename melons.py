ce5ec94458846e98f340e27827d30d0cac57a8fc
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-10 16:55:38 -0700

    refactor: melons.py change dictionary with lists to the dictionary of dictionaries

--- melons.py
+++ melons.py
@@ -1,10 +1,40 @@
 # key = melon_name
-# value = [price, seedlessness, flesh_color, rind_color, average_weight]
+# value = dictionary with keys: price, seedless, flesh_color, rind_color, average_weight
 
 melons = {
-    'Honeydew': [0.99, True, None, None, None],
-    'Crenshaw': [2.00, False, None, None, None],
-    'Crane': [2.50, False, None, None, None],
-    'Casaba': [2.50, False, None, None, None],
-    'Cantaloupe': [0.99, False, None, None, None],
+    'Honeydew': {
+        'price': 0.99,
+        'seedless': True,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    }, 
+    'Crenshaw': {
+        'price': 2.00,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    },
+    'Crane': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    },
+    'Casaba': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    },
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None,
+        'rind_color': None,
+        'average_weight': None,
+    }
 }
