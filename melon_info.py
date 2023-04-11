ebed5a7d8da9111190aba22f548bd958d5faf104
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-10 17:07:28 -0700

    feat: update melon_info.py to print melons from new structure

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,17 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melons
 
 
-def print_melon(name, price, seedless):
+def print_melon(melon_name, melon_attributes):
     """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+    melon_name = melon_name.upper()
+    print(melon_name)
+    for attr, value in melon_attributes.items():
+        print(f"{attr}: {value}")
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+for melon_name, melon_attributes in melons.items():
+    print_melon(melon_name, melon_attributes)
+    print()
